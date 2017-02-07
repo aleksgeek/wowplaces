58a6fbe6a7af5f45a972ee46087395c4893ecb71
aleksgeek <alex.www.coder@gmail.com>
2017-02-07 22:58:46 +0200

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var gulpIf = require('gulp-if');
+
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: 'public-dev'
+		},
+	})
+});
+
+gulp.task('build', function(){
+	gulp.src('public-dev/fonts/*').pipe(gulp.dest('public/fonts'));
+	gulp.src('public-dev/img/*').pipe(gulp.dest('public/img'));
+	gulp.src('public-dev/views/**/*.html').pipe(gulp.dest('public/views'));
+    
+    return gulp.src('public-dev/*.html')
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulpIf('*.css', minifyCSS()))
+		.pipe(useref())
+		.pipe(gulp.dest('public'));
+});
